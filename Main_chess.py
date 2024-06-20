89c005ae3c64157d460a79e40479e81a3aca44a0
moises4zr <vargasgarciamarvinmoises@gmail.com>
2024-06-20 16:35:36 -0600

    Se sube el pryecto

---
+++ Main_chess.py
@@ -0,0 +1,10 @@
+from Piezas import * 
+
+
+def main():
+    rey1 = Rey('Negro', 'no movida', True) 
+    print(rey1.color, rey1.estado, rey1.capturada, rey1.posiciones)  
+
+
+if __name__ == "__main__":
+    main()
