f8feb0d3fe3e17a452ed8c39801ebdd0563a121f
moises4zr <vargasgarciamarvinmoises@gmail.com>
2024-06-20 21:45:21 -0600

    cambios en las clases

--- Main_chess.py
+++ Main_chess.py
@@ -1,8 +1,9 @@
-from Piezas import * 
+from Piezas import *
+from Rey import *
 
 
 def main():
-    rey1 = Rey('Negro', 'no movida', True) 
+    rey1 = rey('Negro', 'no movida') 
     print(rey1.color, rey1.estado, rey1.capturada, rey1.posiciones)  
 
 
--- Piezas.py
+++ Piezas.py
@@ -1,34 +1,12 @@
 class piezas():
-    def __init__(self, color, estado = 'no movida', capturada = False, posiciones = None ): 
+    def __init__(self, color, estado = 'no movida', capturada = False, posiciones = None): 
         self.color = color 
         self.estado = estado
         self.capturada = capturada
         self.posiciones = posiciones
         
-        
-class Rey(piezas): 
-    def __init__(self, color, estado = 'no movida', capturada = False, posiciones = None ): 
-        super().__init__(color, estado,  capturada, posiciones)
 
-class Reina(piezas): 
-    def __init__(self, color, estado, capturada, posiciones):
-        super().__init__(color, estado,  capturada, posiciones)
-        
-class Caballo(piezas): 
-      def __init__(self, color, estado, capturada, posiciones):
-        super().__init__(color, estado,  capturada, posiciones)
-        
-class Alfil(piezas): 
-      def __init__(self, color, estado, capturada, posiciones):
-        super().__init__(color, estado,  capturada, posiciones)
-        
-class Torre(piezas): 
-      def __init__(self, color, estado, capturada, posiciones):
-        super().__init__(color, estado,  capturada, posiciones)
-        
-class Peon(piezas): 
-      def __init__(self, color, estado, capturada, posiciones, promocion):
-        super().__init__(color, estado,  capturada, posiciones)
-        self.promocion = promocion 
+
+    
     
     
