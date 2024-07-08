a917e463b22f9d8d14e06f36a59c31fb1f8e955c
moises4zr <vargasgarciamarvinmoises@gmail.com>
2024-07-07 23:59:50 -0600

    cambios en la clase de peon

--- Peon.py
+++ Peon.py
@@ -8,7 +8,7 @@ class peon(piezas):
         fila_actual, columna_actual = self.posicion
         nueva_fila, nueva_columna = nueva_posicion
 
-        # Lógica de movimiento para el peón: mover una casilla hacia adelante en la misma columna
+        
         if columna_actual == nueva_columna and (nueva_fila == fila_actual + 1):
             self.posicion = nueva_posicion
             print(f"La pieza 'peon' ahora está en la posición: {self.posicion}")
